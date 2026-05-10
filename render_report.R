71d2a6065ff72418c36c64dc34abfced7c9f518c
Victor Lorenzo Lopez <victorlorenzo05@gmail.com>
2026-05-10 22:34:45 +0200

    Remove render_report.R

--- render_report.R
+++
@@ -1,7 +0,0 @@
-## Run this script to produce the Word report from XAI3_report.Rmd
-## Required packages: ranger, pdp, ggplot2, dplyr, knitr, rmarkdown
-
-rmarkdown::render(
-  input       = "XAI3_report.Rmd",
-  output_file = "XAI3_report.docx"
-)
